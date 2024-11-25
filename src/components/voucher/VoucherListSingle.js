3b46a2934ed7693ae5c62e41e20f9e7eb5d0b209
vangpham7624 <phamvangpubg@gmail.com>
2024-11-25 15:02:06 +0700

    fix-voucher

--- src/components/voucher/VoucherListSingle.js
+++ src/components/voucher/VoucherListSingle.js
@@ -9,65 +9,63 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { connect } from "react-redux";
 import { Form, Input, message } from "antd";
 
-const VoucherListSingle = ({ voucher, cartItems }) => {
-  const [modalShow, setModalShow] = useState(false);
-  const { addToast } = useToasts();
-  const history = useHistory();
+const VoucherListSingle = ({
+	voucher,
+	cartItems
+}) => {
+	const [modalShow, setModalShow] = useState(false);
+	const { addToast } = useToasts();
+	const history = useHistory();
 
-  return (
-    <Fragment>
-      <div
-        className={`d-flex voucher-wrap align-items-center border-dashed justify-content-around mb-3`}
-      >
-        <div className="voucher-img">
-          <img
-            className="default-img"
-            src="../assets/img/voucher.png"
-            alt="Voucher"
-            style={{ width: "100%", height: "70px", objectFit: "contain" }}
-            onError={onErrorImage}
-          />
-        </div>
+	return (
+		<Fragment>
+			<div
+				className={`d-flex voucher-wrap align-items-center border-dashed justify-content-around mb-3`}
+			>
+				<div className="voucher-img"
+				>
+					<img
+						className="default-img"
+						src="../assets/img/voucher.png"
+						alt="Voucher"
+						style={{ width: "100%", height: "70px", objectFit: "contain" }}
+						onError={onErrorImage}
+					/>
+				</div>
 
-        <div className="voucher-title">
-          <p>
-            <b>Voucher</b>:{voucher.name}
-          </p>
-          <p>
-            <b>Giảm giá</b>:{voucher.amount} %
-          </p>
-          <p>
-            <b>Giá trị đơn hàng tối thiểu</b>:
-            {customNumber(voucher.minimum, "đ")}
-          </p>
-        </div>
-        <div className=" btn voucher-apdung">
-          <Link
-            className="apvoucher"
-            onClick={() => {
-              if (!cartItems.length) {
-                message.error("Không có sản phẩm nào trong giỏ hàng");
-                return;
-              }
-              history.push("/checkout?voucher=" + voucher.name);
-            }}
-          >
-            Chọn
-          </Link>
-        </div>
-      </div>
-    </Fragment>
-  );
+				<div className="voucher-title">
+					<p><b>Voucher</b>:{voucher.name}</p>
+					<p><b>Giảm giá</b>:{voucher.amount} %</p>
+					<p><b>Giá trị đơn hàng tối thiểu</b>:{customNumber(voucher.minimum, 'đ')}</p>
+				</div>
+				<div className=" btn voucher-apdung">
+					<Link className="apvoucher" onClick={() => {
+						if (!cartItems.length) {
+							message.error( "Không có sản phẩm nào trong giỏ hàng" );
+							return;
+						}
+						history.push("/checkout?voucher=" + voucher.name)
+					}
+
+					}
+					>
+						Chọn
+					</Link>
+				</div>
+			</div>
+
+		</Fragment>
+	);
 };
 
 VoucherListSingle.propTypes = {
-  cartItems: PropTypes.array,
-  voucher: PropTypes.object,
+	cartItems: PropTypes.array,
+	voucher: PropTypes.object,
 };
-const mapStateToProps = (state) => {
-  return {
-    cartItems: state.cartData,
-  };
+const mapStateToProps = state => {
+	return {
+		cartItems: state.cartData,
+	};
 };
 
 export default connect(mapStateToProps)(VoucherListSingle);
