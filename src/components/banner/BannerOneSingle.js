9348d96f6118c2dac157a7b25221f7a7e5a75809
DESKTOP-509LRUB\Duyphandien <bachduy666@gmail.com>
2024-12-04 14:50:59 +0700

    fix/banner

--- src/components/banner/BannerOneSingle.js
+++ src/components/banner/BannerOneSingle.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { Link } from "react-router-dom";
-
+//banner 
 const BannerOneSingle = ({ data, spaceBottomClass }) => {
   return (
     <div className="col-lg-4 col-md-4">
